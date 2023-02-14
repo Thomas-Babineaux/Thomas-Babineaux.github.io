406e22a76b35cae3ea96bc0f38c586cfc003e999
Thomas-Babineaux <tmb7519901@lpssonline.com>
2023-02-14 19:05:48 +0000

    fixed circles git push git add . git add .

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,13 +21,14 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
         var circle;
-        var circles = [] 
+        var circles = [];
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle(){
         circle = draw.randomCircleInArea(canvas, true, false, '#999', 2);
-        physikz.addRandomVelocity(circle, canvas, 20, 20);
+        physikz.addRandomVelocity(circle, canvas, 10, 10);
         view.addChild(circle);
         circles.push(circle);
-
+        }
         // TODO 3 / 7 : Call the drawCircle() function 
 
 
@@ -44,12 +45,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            
+            for (var i = 0; i < circles.length; i++) {
+
+                physikz.updatePosition(circles[i]);
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            
+            for (var i = 0; i < circles.length; i++) {
 
-            // TODO 9 : Iterate over the array
+                game.checkCirclePosition(circles[i]);
+            }
+
+            // TODO 8/9 : Iterate over the array
            for (var eachCircle = 0; eachCircle < circles.length; eachCircle++) {
             var eachValue = circles[eachCircle];
             physikz.updatePosition(circles[eachCircle]);
