70eda1a56ee69fed3e386a6c28de8e5cfbb72d04
Thomas-Babineaux <tmb7519901@lpssonline.com>
2023-10-26 15:59:04 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -2,7 +2,7 @@
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
   
-function runProgram(){
+function runProgram(){}
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -16,8 +16,28 @@ function runProgram(){
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleEvent);
+  function handleKeyDown(event) {
+    console.log(event.keyCode);
+  }
+ // change 'eventType' to the type of event you want to handle
+var KEY = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    A: 65,
+    W: 87,
+    S: 83,
+    D: 68
+};
+var walker = {
+  x: 0,
+  y: 0,
+  SpeedX: 0,
+  speedY: 0
+};
+$(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -26,29 +46,83 @@ function runProgram(){
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
-  function newFrame() {
-    
 
-  }
   
   /* 
   Called in response to events.
+ 
   */
-  function handleEvent(event) {
-
+function handleKeyDown(event) {
+  if (event.which === KEY.LEFT){
+    console.log("left pressed");
+  } else if (event.which === KEY.UP){
+    console.log("up pressed");
+  } else if (event.which === KEY.RIGHT){
+    console.log("Right pressed");
+} else if (event.which === KEY.DOWN){
+  console.log("Down pressed")
+}
+function handleKeyDown(event) {
+  if (event.which === KEY.LEFT){
+    walker.SpeedX = -5;
+    walker.SpeedY = 0;
+  }  else if (event.which === KEY.DOWN){
+    walker.SpeedX = 0;
+    walker.SpeedY = 5;
+  } else if (event.which === KEY.UP){
+    walker.SpeedX = 0;
+    walker.SpeedY = -5;
+  }  else if (event.which === KEY.RIGHT){
+    walker.SpeedX = -5;
+    walker.SpeedY = 0;
+  } 
+}
+function handleKeyUp(event) {
+  if (event.which >= KEY.LEFT && event.which <= KEY.DOWN) {
+    walker.SpeedX = 0;
+    walker.SPeedY = 0;
   }
-
+}
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
   
-  function endGame() {
-    // stop the interval timer
+  function repositionGaneItem() {
+    walker.x += walker.SpeedX;
+    walker.y += walker.speedY;
+    wallCollision();
+   }
+    function redrawGameItem() {
+    $("#walker").css("left". walker.x);
+    $("#walker").css("top", walker.y);
+  }
+    function endGame() {
     clearInterval(interval);
-
+    
+    $(document).off();
+  }
+    function wallCollision(){
+      if (walker.x < 0) {
+        walker.x = 0;
+      }
+      if (walker.y < 0) {
+        walker.y = 0;
+      }
+      if (walker.x + $("#walker").width() > $("#board").width()) {
+        (walker.x + $("#board").width() - $("#walker").width()) ;
+      }
+      if (walker.y + $("#walker").height() > $("#board").height()) {
+        walker.y = $("#board").height() - $("#walker").height();
+      }
+    }
+  function newframe(){
+    repositianGameItem()
+    redrauGameItem();
+    wallcollision();
+  }
+    setInterval(newfrane, 16);
     // turn off event handlers
     $(document).off();
   }
-  
-}
+
